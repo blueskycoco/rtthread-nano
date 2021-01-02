7e79d9236af4fce5164758e60420b97a8e3e8834
fmin-nreal <fmin@nreal.ai>
2021-01-03 06:49:43 +0800

    debug wifi

--- rt-thread/bsp/stm32f030f4p6/main.c
+++ rt-thread/bsp/stm32f030f4p6/main.c
@@ -13,7 +13,19 @@
 struct rt_semaphore sem;
 rt_uint8_t uart_rcv[512];
 rt_uint16_t uart_rcv_len;
+rt_uint8_t state = 0;
 rt_uint8_t flag = 0;
+enum {
+	STATE_INIT,
+	STATE_INIT1,
+	STATE_TCP_LINK,
+	STATE_AP_LINK,
+	STATE_SET_SSID,
+	STATE_SET_KEY,
+	STATE_THROUGH,
+	STATE_STA_MODE,
+	STATE_TCP_CRT,
+};
 void led_init()
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -34,6 +46,44 @@ void enter_at_mode()
 	rt_hw_console_output1("a");
 	rt_thread_mdelay(100);
 }
+void wifi_configure()
+{
+	switch (state) {
+		case STATE_INIT:
+			state = STATE_INIT1;
+			rt_hw_console_output1("at+tcplk\n");
+			break;
+		case STATE_INIT1:
+			state = STATE_TCP_LINK;
+			rt_hw_console_output1("at+tcplk\n");
+			break;
+		case STATE_TCP_LINK:
+			if (strstr(uart_rcv, "on")) {
+				state = STATE_THROUGH;
+				rt_hw_console_output1("at+entm\n");
+			} else {
+				state = STATE_AP_LINK;
+				rt_hw_console_output1("at+wslk\n");
+			}
+			break;
+		case STATE_AP_LINK:
+			if (!strstr(uart_rcv, "Disconnected") && !strstr(uart_rcv, "RF Off")) {
+				state = STATE_TCP_CRT;
+				rt_hw_console_output1("at+tcpdis=on\n");
+			} else {
+				state = STATE_SET_SSID;
+				rt_hw_console_output1("at+wsssid=CMCC-333\n");
+			}
+			break;
+		case STATE_TCP_CRT:
+				state = STATE_INIT1;
+				rt_hw_console_output1("at+netp=TCP,CLIENT,7777,192.168.1.9\n");
+			break;
+		default:
+			rt_hw_console_output1(uart_rcv);
+			break;
+	}
+}
 int main(void)
 {
 
@@ -44,6 +94,8 @@ int main(void)
 	while (1)
     {
 		rt_memset(uart_rcv, 0, 512);
+
+#if 0
 		if (flag == 0 || flag == 1) {
     		flag++;
     		rt_hw_console_output1("at+wsssid=xxx\n");
@@ -57,6 +109,17 @@ int main(void)
 			flag++;
     		rt_hw_console_output1("at+wslq\n");
 		}
+#else
+#if 0
+		if (flag == 0 || flag == 1) {
+    		flag++;
+    		rt_hw_console_output1("at+wmode=sta\n");
+		} else
+    		rt_hw_console_output1("at+z\n");
+#else
+		wifi_configure();
+#endif
+#endif
     	rt_sem_take(&sem, RT_WAITING_FOREVER);
     }
 }
